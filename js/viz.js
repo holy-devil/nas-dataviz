c92f5b92f424edeafdccf32c6db911f9cc828c71
Prasanta Kr Dutta <pkd.dapacific@gmail.com>
2018-03-09 02:36:07 +0530

    Cleaned JS file and removed rounded corners from <kbd>

--- js/viz.js
+++ js/viz.js
@@ -35,66 +35,7 @@ d3.csv('datasets/nas/OverviewCorrelationFinal.csv', function(error, data) {
     // Converting data to labels and numbers from CSV strings
     nas_data = data.map(function (d){
         // console.log(d[corr_headers[1]]); Accessing the data dynamically
-        /*
-        return {
-            Subjects: d.Subjects,
-            Gender: +d.Gender,
-            Age: +d.Age,
-            Category: +d.Category,
-            Samelanguage: +d.Samelanguage,
-            Siblings: +d.Siblings,
-            Handicap: +d.Handicap,
-            Fatheredu: +d.Fatheredu,
-            Motheredu: +d.Motheredu,
-            Fatheroccupation: +d.Fatheroccupation,
-            Motheroccupation: +d.Motheroccupation,
-            Belowpoverty: +d.Belowpoverty,
-            Usecalculator: +d.Usecalculator,
-            UseInternet: +d.UseInternet,
-            Usedictionary: +d.Usedictionary,
-            Readotherbooks: +d.Readotherbooks,
-            Books: +d.Books,
-            Distance: +d.Distance,
-            Computeruse: +d.Computeruse,
-            Libraryuse: +d.Libraryuse,
-            Likeschool: +d.Likeschool,
-            GiveLangHW: +d.GiveLangHW,
-            GiveMathHW: +d.GiveMathHW,
-            GiveScieHW: +d.GiveScieHW,
-            GiveSoScHW: +d.GiveSoScHW,
-            CorrectLangHW: +d.CorrectLangHW,
-            CorrectMathHW: +d.CorrectMathHW,
-            CorrectScieHW: +d.CorrectScieHW,
-            CorrectSocSHW: +d.CorrectSocSHW,
-            HelpinStudy: +d.HelpinStudy,
-            Privatetuition: +d.Privatetuition,
-            Englishisdifficult: +d.Englishisdifficult,
-            ReadEnglish: +d.ReadEnglish,
-            Dictionarytolearn: +d.Dictionarytolearn,
-            AnswerEnglishWB: +d.AnswerEnglishWB,
-            AnswerEnglishaloud: +d.AnswerEnglishaloud,
-            Mathsisdifficult:+d.Mathsisdifficult,
-            SolveMaths: +d.SolveMaths,
-            SolveMathsingroups: +d.SolveMathsingroups,
-            Drawgeometry: +d.Drawgeometry,
-            Explainanswers: +d.Explainanswers,
-            SocSciisdifficult: +d.SocSciisdifficult,
-            Historicalexcursions: +d.Historicalexcursions,
-            ParticipateinSocSci: +d.ParticipateinSocSci,
-            SmallgroupsinSocSci: +d.SmallgroupsinSocSci,
-            ExpressSocSciviews: +d.ExpressSocSciviews,
-            Scienceisdifficult: +d.Scienceisdifficult,
-            Observeexperiments: +d.Observeexperiments,
-            Conductexperiments: +d.Conductexperiments,
-            Solvescienceproblems: +d.Solvescienceproblems,
-            Expressscienceviews: +d.Expressscienceviews,
-            WatchTV: +d.WatchTV,
-            Readmagazine:+d.Readmagazine,
-            Readabook: +d.Readabook,
-            Playgames: +d.Playgames,
-            Helpinhousehold: +d.Helpinhousehold
-        }; 
-        */
+        
         return {
             Subjects: d.Subjects,
             Gender: +d.Gender,
@@ -146,31 +87,6 @@ d3.csv('datasets/nas/OverviewCorrelationFinal.csv', function(error, data) {
     }
     console.log(nas_data);
     // Waypoints controllers
-    // $('#heatmap').waypoint({
-    //     handler: function(direction) {
-    //       console.log(this.element.id + ' hit')
-    //     //   if ($('#subjects svg').length === 0) { // draw the heatmap if the SVG not drawn yet
-    //       heatmapSub(nas_data);
-    //     //   heatmap(nas_data,1);
-    //       this.destroy(); // destroy the waypoint after once trigger
-    //     //   }
-    //     },
-    //     // context: '#overflow-scroll-offset',
-    //     offset: '96%'
-    //   })
-    //   $('#intro-row').waypoint({
-    //     handler: function(direction) {
-    //       console.log(this.element.id + ' hit')
-    //     //   if ($('#studplot svg').length === 0) // draw the heatmap if the SVG not drawn yet
-    //       studPlot(nas_data);
-    //       $('#intro-row .alert').removeClass("hidden");
-    //     //   if ($('#studsubplot svg').length === 0) // draw the heatmap if the SVG not drawn yet
-    //       studSubPlot(nas_data);
-    //       this.destroy();
-    //     },
-    //     // context: '#overflow-scroll-offset',
-    //     offset: '96%'
-    //   })  
     var stickyHeat = new Waypoint.Sticky({
         element: $('#heatFilters'),
         offset: '0%'
@@ -265,13 +181,7 @@ function heatmap (data,id) {
             rowLabels.on("mouseover", rowMouseOver).on("mouseout", rowMouseOut);                    
             
             let heatMap = svgContainer.append("g").attr("class","heatg");
-            //.data(data)
-            // .enter()
-            // .append("circle")
-            // .attr("cx", function(d) { return marginX+(cellw/2); })
-            // .attr("cy", function(d, i) { return marginY+cellh*(i+1)*2; })
-            // .attr("class", function(d){return "cell cell-border"})
-            // .attr("r", max*50);
+     
             // build the heatmap
             for (i=0; i<data.length; i++)
             {
